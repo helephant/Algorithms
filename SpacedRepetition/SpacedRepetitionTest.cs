408b0f6756f6fba518eafdd43c3d74a03ed03580
Helen Emerson <helen@helephant.com>
2013-09-11 13:16:16 +0100

    Creating project to try to model spaced repetition algorithm

---
+++ SpacedRepetition/SpacedRepetitionTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SpacedRepetitionAlgorithm
+{
+    [TestFixture]
+    public class SpacedRepetitionTest
+    {
+        [Test]
+        public void WhenCardIsCorrectSpacingIncreases()
+        {
+            var session = new SpacedRepetitionSession();
+            var now = DateTime.Now;
+            session.Add(new Fact("neko", "cat", now.AddHours(-1), now));
+
+            var fact = session.Next();
+            fact.Answer(Answer.Correct);
+
+            Assert.That(fact.NextReview, Is.EqualTo(now.AddHours(2)));
+        }
+    }
+
+    public enum Answer
+    {
+        Correct, 
+        Incorrect
+    }
+
+    public class SpacedRepetitionSession 
+    {
+        readonly List<Fact> _facts = new List<Fact>();
+
+        public void Add(Fact newFact)
+        {
+            _facts.Add(newFact);
+        }
+
+        public Fact Next()
+        {
+            return _facts[0];
+        }
+    }
+
+    public class Fact
+    {
+        public Fact(string expression, string meaning, DateTime lastReview, DateTime nextReview)
+        {
+            Expression = expression;
+            Meaning = meaning;
+            LastReview = lastReview;
+            NextReview = nextReview;
+        }
+
+        public void GiveAnswer(Answer answer)
+        {
+            if (answer == Answer.Correct)
+            {
+                NextReview = DateTime.Now.Add(LastReview - DateTime.Now);
+                LastReview = DateTime.Now;
+            }
+        }
+
+        public string Expression { get; private set; }
+        public string Meaning { get; private set; }
+
+        public DateTime NextReview { get; private set; }
+        public DateTime LastReview { get; private set; }
+    }
+}
---
+++ SpacedRepetition/readme.md
@@ -0,0 +1,11 @@
+﻿A spike to explore the spaced repetition algorithm. 
+
+Spaced repetition is a learning technique that works by showing facts at increasingly large intervals. 
+
+The first time you get a fact right, it will be shown after an interval of n. The second time you get it right, the interval before showing the same fact again will be 2n. The third time the interval will be 4n. If you get the fact wrong, the interval is reset to zero and you build up the intervals again.
+
+The theory is that ideally you want to review something the moment before you forget it so that you reinforce the knowledge without wasting time practicing things you still remember. If you have a fact that you have remembered for five days, you should try waiting longer next time to see if you can still access it. Each time you remind yourself, you form a stronger memory. 
+
+Spaced repetition is useful for learning large numbers of facts. I came across it when learning Japanese vocabularly, particularly when learning to recognise the kanji pictorial characters. I practiced about three times a week for half an hour for about two years and knew about 1000 words at the peak of my practice. Now 18 months later, I still know about 500 very well and another 200 with prompting. 
+
+There are a few common spaced repetition algorithms. I'm going to try Super Memo. 
